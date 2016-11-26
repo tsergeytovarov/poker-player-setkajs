c4498b07ac0fe17d0938a809275f8767f28e0fd8
Yegor Yakovishen <yakovishen@setka.io>
2016-11-26 13:14:03 +0300

    has same

--- Player.js
+++ Player.js
@@ -1,26 +1,80 @@
+const hasSameSuits = require('./lib/hasSameSuits');
+const format = require('./lib/format');
+const analyze = require('./lib/format');
+
 class Player {
   static get VERSION() {
-    const version = 1.2;
+    const version = 2;
     return `v${version}`;
   }
 
   static betRequest(gameState) {
     const { current_buy_in } = gameState;
     const myPlayer = gameState.players[gameState.in_action];
-    const rand = Math.random();
-
     let bet;
-    console.log(rand);
 
-    // Call
-    if (rand < 0.5) {
-      bet = current_buy_in - myPlayer.bet;
-    }
+    const round = game_state.round;
 
-    // Raise
-    else {
-      bet = (current_buy_in - myPlayer.bet) + gameState.minimum_raise;
+switch (round) {
+  case 0: {
+    // call
+    return;
+  }
+
+  case 1 {
+    if (isPair || isThree || hasFour) {
+      // call
+    } else if (isFour || Four) {
+      // allin
+    } else {
+      return 0;
     }
+    return;
+  }
+
+  case 2: {
+    if (isPair || isThree || SameSuite(4)) {
+      // call
+    } else if (isFour || SameSuite(5) ) {
+      // allin
+    } else {
+      return 0;
+    }
+    return;
+  }
+
+  case 3: {
+    if (isPair || isThree || SameSuite(4)) {
+      // call
+    } else if (isFour || SameSuite(5) ) {
+      // allin
+    } else {
+      return 0;
+    }
+    return;
+  }
+
+  case 4: {
+    if (isPair || isThree) {
+      // call
+    } else if (isFour || SameSuite(5) ) {
+      // allin
+    } else {
+      return 0;
+    }
+  }
+
+}
+
+    // // Call
+    // if (rand < 0.5) {
+    //   bet = current_buy_in - myPlayer.bet;
+    // }
+    //
+    // // Raise
+    // else {
+    //   bet = (current_buy_in - myPlayer.bet) + gameState.minimum_raise;
+    // }
 
     return bet;
   }
---
+++ test/hasPairs.test.js
@@ -0,0 +1,11 @@
+const Player = require('../Player');
+
+console.log(Player);
+
+const cards = [
+  { ranks: '2' },
+  { ranks: '4' },
+  { ranks: 'A' },
+];
+
+// console.log(hasPairs(cards));
