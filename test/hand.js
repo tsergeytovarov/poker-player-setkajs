3b0796e57a31cb1ea0841cef172e541815a13715
Aleksandr Yakunichev <canvaskisa@gmail.com>
2016-11-26 16:00:14 +0300

    Update poker-evaluator

--- test/hand.js
+++ test/hand.js
@@ -26,7 +26,28 @@ const cardsPair = [{
 }, {
   "rank": "A",
   "suit": "spades"
-}]
+}];
+
+const cardsSix = [{
+  "rank": "6",
+  "suit": "hearts"
+}, {
+  "rank": "Q",
+  "suit": "spades"
+}, {
+  "rank": "K",
+  "suit": "hearts"
+}, {
+  "rank": "K",
+  "suit": "spades"
+}, {
+  "rank": "A",
+  "suit": "spades"
+}, {
+  "rank": "9",
+  "suit": "spades"
+}];
 
 console.log(hand(cardsHigh));
 console.log(hand(cardsPair));
+console.log(hand(cardsSix));
